2371b76fdf4995f282d14e10b0556a99c40c3bba
vairavan6 <vairavan.u@gmail.com>
2018-11-27 23:17:10 +0530

    refs #master inital commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FRONT_END_PATH = './front/';
+const FRONT_END_SRC_PATH = './front/src/';
+
+module.exports = {
+   entry: FRONT_END_SRC_PATH+'./root.js',
+   output: {
+      path: path.join(__dirname, FRONT_END_PATH+'static'),
+      filename: 'foryou_bundle.js'
+   },
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+   module: {
+      rules: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   },
+   plugins:[
+      new HtmlWebpackPlugin({
+         template: FRONT_END_PATH+'dashboard.html'
+      })
+   ]
+}
