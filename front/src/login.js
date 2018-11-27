d53a1745142c9646de00ef6982b16318221ee15a
vairavan6 <vairavan.u@gmail.com>
2018-11-27 23:50:26 +0530

    refs #master antd css check

--- front/src/login.js
+++ front/src/login.js
@@ -1,11 +1,55 @@
-import React, { Component } from 'react';
-class App extends Component{
-   render(){
-      return(
-         <div>
-            <h1>Hello World</h1>
-         </div>
-      );
-   }
+import React from 'react';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import './styles/form.css';
+
+const FormItem = Form.Item;
+
+class LoginContainer extends React.Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <FormItem>
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(
+            <Checkbox>Remember me</Checkbox>
+          )}
+          <a className="login-form-forgot" href="">Forgot password</a>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </FormItem>
+      </Form>
+    );
+  }
 }
-export default App;
+
+const WrappedLoginContainer = Form.create()(LoginContainer);
+export default WrappedLoginContainer;
--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,13 @@ module.exports = {
             exclude: /node_modules/,
             loader: 'babel-loader',
             query: {
-               presets: ['es2015', 'react']
+               presets: ['es2015', 'react'],
+               plugins: ['transform-class-properties']
             }
+         },
+         {
+        	 test: /\.css$/,
+        	 use: [ 'style-loader', 'css-loader' ]
          }
       ]
    },
