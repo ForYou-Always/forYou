8c56a7e21a69cd02ba9d967f02db1d1054a63b5a
Praveen kumar <erodepraveen94@gmail.com>
2019-01-13 09:17:34 +0530

    Newsfeed integrated to new folder structure

---
+++ front/src/home/layout/newsfeed/news-feed-screen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import NewsFeed from './news-feed';
+import { Layout, Avatar, Card, Row, Col, Input, Button, notification} from 'antd';
+import getData from './json-data';
+import PicturesWall from './pictures-wall';
+const { Header, Sider, Content } = Layout;
+const { Meta } = Card;
+const TextArea = Input.TextArea;
+
+class NewsFeedScreen extends React.Component{
+    render(){
+        const data = getData();
+        const userDetails = {
+            loginUserDetails:{
+                id: 'praveen',
+                src: "./front/src/styles/images/userImg1.png",
+                name: 'Praveen kumar'
+            }
+        };
+        const headerColor = {
+            background: 'rgb(215, 182, 245)'
+        };
+        const siderColor = {
+            padding: '8px 8px 8px 8px',
+            background: 'rgb(199, 206, 216)'
+        };
+        const padding = {
+            padding: '8px 8px 8px 8px'
+        };
+        const clickPostButton = () => {
+            if(!this.refs.postTextArea.textAreaRef.value){
+                notification.error({
+                    message: 'Post Message is Empty'
+                });
+            } else {
+                
+            }
+        // console.log(this.refs.refPicturesWall.getFileList());
+        }
+        return (
+            <div>
+                <Card style={padding}>
+                    <Row type="flex" justify='start' align='middle'>
+                        <Col span={22}>
+                            <Meta style={padding}
+                                avatar={<Avatar src={userDetails.loginUserDetails.src}/>}
+                                title={<TextArea ref='postTextArea' placeholder='Whats in your mind.'
+                                autosize={{ minRows: 2, maxRows: 6 }}/>}/>
+                        </Col>
+                        <Col span={2}>
+                            <Button type='primary' onClick={clickPostButton.bind(this)}>Post</Button>
+                        </Col>
+                    </Row>
+                    <PicturesWall ref='refPicturesWall'></PicturesWall>
+                </Card>
+                {data.map((value,index) => <NewsFeed key={index} value={value}></NewsFeed>)}
+            </div>
+        )
+    }
+}
+
+export default NewsFeedScreen;
--- server/src/dbStore/schemaModel/newsfeed-schema.js
+++ server/src/dbStore/schemaModel/newsfeed-schema.js
@@ -1,49 +1,41 @@
 var data = {
-    id: {
-        loginUserDetails:{
+    userDetails:{
+        id: String,
+        src: String,
+        name: String
+    },
+    description: String,
+    files: {
+        id: String,
+        src: String,
+        contentType: String
+    },
+    comment: {
+        id: String,
+        childIds: List,
+        commentContent: String,
+        authorId: String,
+        authorSrc: String,
+        time: { type: Date, default: Date.now },
+        likedBy: List,
+        dislikedBy: List
+    },
+    childCommentData : {
+        childId:{
             id: String,
-            src: String,
-            name: String
-        },
-        userDetails:{
-            id: String,
-            src: String,
-            name: String
-        },
-        subject: String,
-        description: String,
-        files: {
-            id: String,
-            src: String,
-            contentType: String
-        },
-        comment: {
-            id: String,
-            childId: String,
+            childIds: List,
             commentContent: String,
             authorId: String,
             authorSrc: String,
-            time: Date,
-            likedBy: [],
-            dislikedBy: []
-        },
-        childCommentData : {
-            childId:{
-                id: String,
-                childId: String,
-                commentContent: String,
-                authorId: String,
-                authorSrc: String,
-                time: Date,
-                likedBy: [],
-                dislikedBy: []
-            }
-        },
-        uploadTime: Date,
-        modifiedTime: Date,
-        likedBy: [],
-        dislikedBy: [],
-        sharedBy: [],
-        tagedUsers: []
+            time: { type: Date, default: Date.now },
+            likedBy: List,
+            dislikedBy: List
         }
-    }
+    },
+    uploadTime: Date,
+    modifiedTime: { type: Date, default: Date.now },
+    likedBy: List,
+    dislikedBy: List,
+    sharedBy: List,
+    tagedUsers: List
+}
