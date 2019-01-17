98c5933271946a5b69443b5de082499400ea4a3a
Praveen kumar <erodepraveen94@gmail.com>
2019-01-17 07:47:15 +0530

    Schema for newsfeed update commit

---
+++ server/src/dbStore/schemaModel/newsFeedSchema.js
@@ -0,0 +1,54 @@
+const { mongORM } = require('../connection');
+const { Schema, model } = mongORM;
+const CONSTANTS = require('./collectionConst');
+
+const newsFeedSchema = new Schema({
+    userDetails:{
+        id: {type : String, default : ''},
+        src: {type : String, default : ''},
+        name: {type : String, default : ''}
+    },
+    description: {type : String, default : ''},
+    files: {
+        id: {type : String, default : ''},
+        src: {type : String, default : ''},
+        contentType: {type : String, default : ''}
+    },
+    comment: {
+        id: {type : String, default : '-1'},
+        childIds: {type : Array, default : []},
+        commentContent: {type : String, default : ''},
+        authorId: {type : String, default : ''},
+        authorSrc: {type : String, default : ''},
+        time: { type: Date, default: Date.now },
+        likedBy: {type : Array, default : []},
+        dislikedBy: {type : Array, default : []}
+    },
+    childCommentData : {
+        childId:{
+            id: {type : String, default : ''},
+            childIds: {type : Array, default : []},
+            commentContent: {type : String, default : ''},
+            authorId: {type : String, default : ''},
+            authorSrc: {type : String, default : ''},
+            time: { type: Date, default: Date.now },
+            likedBy: {type : Array, default : []},
+            dislikedBy: {type : Array, default : []}
+        }
+    },
+    uploadTime: Date,
+    modifiedTime: { type: Date, default: Date.now },
+    likedBy: {type : Array, default : []},
+    dislikedBy: {type : Array, default : []},
+    sharedBy: {type : Array, default : []},
+    tagedUsers: {type : Array, default : []}
+});
+
+/*
+ * Define Models
+ **/
+const NewsFeedModel = model(CONSTANTS.NEWS_FEED, newsFeedSchema);
+
+module.exports={
+    NewsFeedModel,
+ };
