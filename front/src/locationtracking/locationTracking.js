253b7269dca0bf68b80f6d4443e083809e6b8afc
shashank krishnam <shashank.krishnam@gmail.com>
2019-01-18 22:47:27 +0530

    refs #fy9-tracking tracking code current location commit.

--- front/src/locationtracking/locationTracking.js
+++ front/src/locationtracking/locationTracking.js
@@ -1,80 +1,49 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import {geolocated} from 'react-geolocated';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import CurrentLocation from './Map';
 
+class LocationTracking extends React.Component {
+	state = {
+		    showingInfoWindow: false,
+		    activeMarker: {},
+		    selectedPlace: {}
+		  };
 
-
-
-class LocationTracking extends Component {
-
-	// The constructor takes properties defined as element attributes
-	  // defined in JSX along with an initial default value for state.
-
-	  constructor(props) {
-	    super(props);
-	    this.state = {
-	      value: '0,0', // Null Island
-	      error: null,
-	    }
-	  }
-	  
-	  componentDidMount() {
-		    if (navigator.geolocation) {
-		      navigator.geolocation.getCurrentPosition(
-		        (position) => {
-		          this.setState({
-		            value: position.coords.latitude + ',' + position.coords.longitude,
-		            latitude: position.coords.latitude,
-		            longitude: position.coords.longitude,
-		            error: null,
-		          });
-		        },
-		        (error) => this.setState(
-		          {error: error.message}
-		        )
-		      );
-		    }
-		  }
-	  
-	  changeLocation(evt) {
+		  onMarkerClick = (props, marker, e) =>
 		    this.setState({
-		        value: evt.target.value,
-		        latitude: position.coords.latitude,
-	            longitude: position.coords.longitude,
-		      }
-		    )
-		  }
-	  
-	 render() {
-		 const style = {
-				  width: '100%',
-				  height: '100%'
-				}
-		 
+		      selectedPlace: props,
+		      activeMarker: marker,
+		      showingInfoWindow: true
+		    });
+
+		  onClose = props => {
+		    if (this.state.showingInfoWindow) {
+		      this.setState({
+		        showingInfoWindow: false,
+		        activeMarker: null
+		      });
+		    }
+		  };
+
+		  render() {
 		    return (
-		    		<div>
-		    		<input
-		              className="new-todo"
-		              value={ this.state.value }
-		              onChange={ evt => this.changeLocation(evt) }
-		              />
-		             
-		    		<Map google={this.props.google}  style={style}
-		    		initialCenter={{
-		    			lat: this.state.latitude,
-		    			lng: this.state.longitude
-//		    			lat: 40.854885,
-//		                lng: -88.081807
-		    		}} zoom={14}>
-		    		
-		    		<Marker onClick={this.onMarkerClick}
-		    		name={'Current location'} />
-		    		
-		    		<InfoWindow onClose={this.onInfoWindowClose} />
-		    		</Map>
-		      </div>
+		      <CurrentLocation
+		        centerAroundCurrentLocation
+		        google={this.props.google}
+		      >
+		        <Marker onClick={this.onMarkerClick} name={'current location'} />
+		        <InfoWindow
+		          marker={this.state.activeMarker}
+		          visible={this.state.showingInfoWindow}
+		          onClose={this.onClose}
+		        >
+		          <div>
+		            <h4>{this.state.selectedPlace.name}</h4>
+		          </div>
+		        </InfoWindow>
+		      </CurrentLocation>
 		    );
 		  }
 }
