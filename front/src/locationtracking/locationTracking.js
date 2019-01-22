d733faaea1df19e377359f9944368c63ec1bd413
shashank krishnam <shashank.krishnam@gmail.com>
2019-01-22 22:03:28 +0530

    refs #fy9-tracking alignment & map file name change commit.

--- front/src/locationtracking/locationTracking.js
+++ front/src/locationtracking/locationTracking.js
@@ -2,50 +2,50 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import CurrentLocation from './Map';
+import CurrentLocation from './currentLocation';
 
-class LocationTracking extends React.Component {
-	state = {
-		    showingInfoWindow: false,
-		    activeMarker: {},
-		    selectedPlace: {}
-		  };
+class LocationTracking extends Component {
+  state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+  };
 
-		  onMarkerClick = (props, marker, e) =>
-		    this.setState({
-		      selectedPlace: props,
-		      activeMarker: marker,
-		      showingInfoWindow: true
-		    });
+  onMarkerClick = (props, marker, e) =>
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
 
-		  onClose = props => {
-		    if (this.state.showingInfoWindow) {
-		      this.setState({
-		        showingInfoWindow: false,
-		        activeMarker: null
-		      });
-		    }
-		  };
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
 
-		  render() {
-		    return (
-		      <CurrentLocation
-		        centerAroundCurrentLocation
-		        google={this.props.google}
-		      >
-		        <Marker onClick={this.onMarkerClick} name={'current location'} />
-		        <InfoWindow
-		          marker={this.state.activeMarker}
-		          visible={this.state.showingInfoWindow}
-		          onClose={this.onClose}
-		        >
-		          <div>
-		            <h4>{this.state.selectedPlace.name}</h4>
-		          </div>
-		        </InfoWindow>
-		      </CurrentLocation>
-		    );
-		  }
+  render() {
+    return (
+        <CurrentLocation
+          centerAroundCurrentLocation
+          google={this.props.google}
+        >
+          <Marker onClick={this.onMarkerClick} name={'current location'} />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            onClose={this.onClose}
+          >
+            <div>
+              <h4>{this.state.selectedPlace.name}</h4>
+            </div>
+          </InfoWindow>
+        </CurrentLocation>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
